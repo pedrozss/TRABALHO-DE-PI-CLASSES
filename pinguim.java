22a13d80e601a3cfc7296342ad95609f55e6f28b
Pedro Henrique Rodrigues Soares <102332660+pedrozss@users.noreply.github.com>
2022-09-09 03:21:15 -0300

    Create pinguim.java

---
+++ pinguim.java
@@ -0,0 +1,61 @@
+package pedropi;
+
+public class pinguim { private String cor = "preta e branco";
+    private String especie = "ave";
+    private String pinguim;
+    private String nome;
+    
+    
+    public pinguim(String nomepinguim) {
+       
+       this.nome = nomepinguim;
+    }
+    
+    public pinguim() {
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String get() {
+        return pinguim;
+    }
+
+    public void setpinguim(String pinguim) {
+        this.pinguim = pinguim;
+    }
+    
+    public void andar() {
+        System.out.println("andar");
+    }
+    
+    public void reagrupar() {
+        System.out.println("juntar se ao grupo");
+    }
+    
+    public void nadar() {
+        System.out.println("nadar");
+    }   
+}
