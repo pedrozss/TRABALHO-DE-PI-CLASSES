18a26421e50a5cec6dab5ed0d61a7a1326e0fe0d
Pedro Henrique Rodrigues Soares <102332660+pedrozss@users.noreply.github.com>
2022-09-09 03:20:27 -0300

    Update and rename pinguim to main.java

---
+++ main.java
@@ -0,0 +1,9 @@
+package pedropi;
+
+public class main {
+     public static void main(String[] args) {
+        pinguim pew = new pinguim("Pew");
+      
+        System.out.println("Nome é " + pew.getNome());     
+    }
+}
